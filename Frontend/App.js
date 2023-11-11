a1802f34dc5b3e2afabba3178017b7fab2045639
Diego <garciadiee_@hotmail.com>
2023-11-11 09:19:47 -0300

    web

--- Frontend/App.js
+++ Frontend/App.js
@@ -3,14 +3,15 @@ import Routes from './Routes/index.js';
 //En app.js importamos la posibilidad de "scroll"
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
-export default App = () => {
-  
+const App = () => {
   return (
     <>
-      <GestureHandlerRootView style={{ flex:1 }}>
-      <Routes/> 
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <Routes />
       </GestureHandlerRootView>
     </>
   );
 };
+
+export default App;
 
