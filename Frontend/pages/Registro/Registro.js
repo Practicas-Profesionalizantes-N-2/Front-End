3659859c720eee1b867de55941b16483dc8199ac
Jere-c <jere.carrera.072@gmail.com>
2023-11-30 17:47:37 -0300

    Merge

--- Frontend/pages/Registro/Registro.js
+++ Frontend/pages/Registro/Registro.js
@@ -1,23 +1,4 @@
 import React, { useContext, useState } from 'react';
-<<<<<<< HEAD
-
-// Importamos
-import { Modal, Text, TextInput, TouchableOpacity, View } from 'react-native'
-import { Boton } from '../../estilos/Boton';
-import { InputStyles } from '../../estilos/Input';
-import { Contenedor } from '../../estilos/Container';
-import { TituloCabecera } from '../../estilos/Titulo';
-
-// Habilita la navegacion hacia otras pantallas
-import { useNavigation } from '@react-navigation/native';
-import { ScrollView } from 'react-native-gesture-handler';
-
-// Import para manejar los temas.
-import { ThemeProvider } from 'styled-components';
-import { ThemesContext } from '../../Routes';
-
-import { URLregister } from '../../Routes/url';
-=======
 import { TextInput, View, Modal, Text, TouchableOpacity } from 'react-native';
 import { Boton } from '../../estilos/Boton';
 import { InputStyles } from '../../estilos/Input';
@@ -42,7 +23,6 @@ const validatePassword = (password) => {
     }
     return '';
 };
->>>>>>> Micaela
 
 export default function Registro() {
 
@@ -51,60 +31,6 @@ export default function Registro() {
 
     // Navegacion entre pantallas
     const navigation = useNavigation();
-<<<<<<< HEAD
-
-    // UseState para guardar los datos
-    const [name, setName] = React.useState('');
-    const [lastname, setLastName] = React.useState('');
-    const [email, setEmail] = React.useState('');
-    const [password, setPassword] = React.useState('');
-    const [age, setAge] = React.useState('');
-
-    // Modal
-    // Iniciador 
-    const [modalVisible, setModalVisible] = useState(false);
-    // Mensaje 
-    const [modalMessage, setModalMessage] = useState('');
-
-    const handleRegistro = async () => {
-
-        const data = { name: name, lastname: lastname, email: email, password: password, age: age };
-
-        // Realizar petición fetch
-        try {
-            const response = await fetch(URLregister, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(data)
-            })
-
-            // Validacion BACKEND check
-            if (response.status == 400) { // Si los datos estan bien
-                const errorData = await response.json();
-                for (const error of errorData.msg.errors) {
-                    setModalMessage(error.msg);
-                    setModalVisible(true);
-                }
-            }
-            // Validacion BACKEND validator
-            if (response.status == 401) { // Si el correo ya esta registrado
-                const errorData = await response.json(); {
-                    setModalMessage(errorData.msg);
-                    setModalVisible(true);
-                }
-            }
-            const responseData = await response.json();
-            // Si no encuentra otro registro con ese mail procede a registrar el usuario
-            // Setea en blanco los campos para un proximo registro
-            setName(''), setEmail(''), setAge(''), setPassword(''), setLastName('')
-            // Navega a la siguiente pagina
-            navigation.push('Inicio');
-
-        } catch (error) {
-        }
-=======
     const [nombre, onChangeNombre] = React.useState('');
     const [apellido, onChangeApellido] = React.useState('');
     const [email, onChangeEmail] = React.useState('');
@@ -131,7 +57,6 @@ export default function Registro() {
         // Placeholder para el 'envío de datos' y navegación a la siguiente página
         console.log('Registro enviado');
         navigation.push('Inicio');
->>>>>>> Micaela
     };
 
 
@@ -167,33 +92,6 @@ export default function Registro() {
                         placeholder="Contraseña"
                         secureTextEntry={true}
                     />
-<<<<<<< HEAD
-                    <TextInput
-                        style={InputStyles.input}
-                        onChangeText={(text) => setAge(text)}
-                        value={age}
-                        placeholder="Edad"
-                        keyboardType="numeric" />
-                    <Boton onPress={() => handleRegistro()} > Registrarme </Boton>
-                    <Boton onPress={() => navigation.push('Nosotros')}> Ir a Nosotros </Boton>
-                </View>
-
-                <Modal visible={modalVisible} transparent={true} animationType="fade">
-
-                    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'rgba(0, 0, 0, 0.5)' }}>
-                        <View style={{ backgroundColor: 'white', padding: 20, borderRadius: 10, alignItems: 'center' }}>
-                            <Text>{modalMessage}</Text>
-
-                            <TouchableOpacity onPress={() => setModalVisible(false)} style={{ borderRadius: 10, padding: 10, marginTop: 20, backgroundColor: "lightgrey" }}>
-                                <Text>Cerrar</Text>
-                            </TouchableOpacity>
-
-                        </View>
-
-                    </View>
-                </Modal>
-
-=======
 
 
                     <Boton onPress={() => handleRegistro()}>Registrarme</Boton>
@@ -210,12 +108,7 @@ export default function Registro() {
                         </View>
                     </View>
                 </Modal>
->>>>>>> Micaela
             </ScrollView>
         </ThemeProvider>
     )
 }
-<<<<<<< HEAD
-
-=======
->>>>>>> Micaela
