e6f67dddb8116111fbc2cdaf9ddd2eb53c695477
micaaranguiz <micaelaaranguiz2@gmail.com>
2024-11-01 13:53:41 -0300

    Quedó todo combinado perfectamente

--- Frontend/pages/Bienvenida/Bienvenida.js
+++ Frontend/pages/Bienvenida/Bienvenida.js
@@ -32,12 +32,11 @@ const Bienvenida = () => {
                 <TituloCabecera/>
                     <View style={Contenedor.containerdentro}>
                         <TituloBienvenida> Mi Educación Sexual Integral</TituloBienvenida>
-                        <Logo style={styled.fotos} source={require('../../assets/mesiLogo.jpeg')} />
-                        <Boton onPress={() => navigation.navigate('Login')}>Iniciar Sesión</Boton>
-                        <Boton onPress={() => navigation.push('Nosotros')}>Nosotros</Boton>
+                        <Logo style={styled.fotos} source={require('../../assets/mesiLogo.jpeg')} />                    
                         <Boton onPress={() => navigation.navigate('Login')}> Profesor</Boton>
                         <Boton onPress={() => navigation.navigate('Metaverso')}>Alumno</Boton>
                         <Boton onPress={() => navigation.navigate('Filtros')}>Accesibilidad</Boton>
+                        <Boton onPress={() => navigation.push('Nosotros')}>Nosotros</Boton>
 
                     </View>
                 </ScrollView>
